c9f8be8caebac41c606625da7b97632b720bcf88
unkn0wnd.fbx <titouan.helbert@gmail.com>
2024-12-06 07:33:40 +0100

    responsive finish

--- src/components/header_footer/headerAccueil.tsx
+++ src/components/header_footer/headerAccueil.tsx
@@ -20,15 +20,15 @@ const HeaderAccueil: React.FC<HeaderProps> = ({ }) => {
     }
 
     return (
-        <header className="bg-none backdrop-blur p-4 md:p-6 flex flex-col md:flex-row md:justify-start md:gap-x-16 gap-0 justify-center items-center md:items-start px-4 md:px-16 fixed top-0 left-0 w-full z-50">
-            <div className="flex flex-col md:flex-row items-center justify-between">
+        <header className="bg-none md:backdrop-blur p-4 md:p-6 flex flex-col md:flex-row justify-start md:gap-x-16 gap-0 items-center md:items-start px-4 md:px-16 fixed top-0 md:gap-y-0 gap-y-4 left-0 w-full md:h-auto h-full z-50">
+            <div className="flex flex-row items-center justify-between w-full md:w-auto">
                 <h1 className="text-xl md:text-2xl font-bold text-normal-blue text-center md:text-left">Nuit de l'info 2024 | Limit Breker</h1>
                 <button className="md:hidden text-2xl" onClick={toggleMenu}>
                     {isMenuOpen ? <FaTimes /> : <FaBars />}
                 </button>
             </div>
-            <div className={`flex-col md:flex-row items-center justify-center gap-y-4 md:gap-y-0 gap-x-4 md:gap-x-16 ${isMenuOpen ? 'flex' : 'hidden'} md:flex`}>
-                <div className="flex flex-wrap items-center justify-center gap-x-2 md:gap-x-5 bg-light-gray/40 px-3 md:px-5 py-2.5 md:py-2 rounded-jb">
+            <div className={`flex-col md:flex-row md:h-auto h-full w-full md:w-auto items-center justify-center gap-y-4 md:gap-y-0 gap-x-4 md:gap-x-16 ${isMenuOpen ? 'flex' : 'hidden'} md:flex`}>
+                <div className="flex flex-col md:flex-row w-full md:w-auto flex-wrap items-center justify-center gap-x-2 md:gap-x-5 bg-light-gray/40 backdrop-blur md:backdrop-blur-none px-3 md:px-5 py-2.5 md:py-2 rounded-jb md:h-auto h-full gap-y-4 md:gap-y-0">
                     <Link to="/" className={`${location.pathname === "/" ? "text-normal-blue" : "text-black"} hover:text-normal-blue transition-colors font-Lato font-semibold text-sm md:text-base`}>Accueil</Link>
                     <Link to="/credit" className={`${location.pathname === "/credit" ? "text-normal-blue" : "text-black"} hover:text-normal-blue transition-colors font-Lato font-semibold text-sm md:text-base`}>Crédits</Link>
                     <Link to="/captcha" className={`${location.pathname === "/captcha" ? "text-normal-blue" : "text-black"} hover:text-normal-blue transition-colors font-Lato font-semibold text-sm md:text-base`}>Captcha</Link>
@@ -37,7 +37,7 @@ const HeaderAccueil: React.FC<HeaderProps> = ({ }) => {
                     <Link to="/port" className="text-black hover:text-normal-blue transition-colors font-Lato font-semibold text-sm md:text-base">Retrogaming</Link>
                 </div>
             </div>
-            <div className="mt-4 md:ml-auto md:mt-0 flex justify-center md:justify-end w-full md:w-auto">
+            <div className={`mt-4 md:ml-auto md:mt-0 md:flex ${isMenuOpen ? 'flex' : 'hidden'} justify-center md:justify-end w-full md:w-auto`}>
                 <Confirm title="C'est parti !" couleur="blue" onClick={dialogue} />
             </div>
         </header>
