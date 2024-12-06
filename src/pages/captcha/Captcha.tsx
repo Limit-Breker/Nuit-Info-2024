a6ca03eebcd51a1720677b1acaa9c3d57a3b76ba
Sofia Gribanova <grisoffa@gmail.com>
2024-12-06 03:51:11 +0100

    essaye

--- src/pages/captcha/Captcha.tsx
+++ src/pages/captcha/Captcha.tsx
@@ -1,23 +1,19 @@
 import React from "react";
-
 const MyComponent: React.FC = () => {
   const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
     if (e.key === "Enter") {
       console.log("Enter key pressed: you can run a function instead of this log statement");
     }
-    if (e.key === "ArrowDown") {
-      alert("blob");
+    if (e.key === "ArrowRight") {
+      alert("right");
+    }
+    if (e.key === "ArrowLeft") {
+      alert("left");
     }
   };    
 
   return (
-    <div 
-      className="my-class" 
-      tabIndex={0} // Permet à la div de recevoir le focus et de capturer les événements clavier.
-      onKeyDown={handleKeyDown}
-    >
-        blob
-    </div>
+    <img src="/images/captcha/boat.png" alt="haha" tabIndex={0} onKeyDown={handleKeyDown}/>
   );
 };
 
