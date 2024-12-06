cc72242f79e7c9bea68b6b90e97f71406f380737
Cemailla <celia.tognini@gmail.com>
2024-12-06 06:57:37 +0100

    LE POSTCAAAAAST

--- src/pages/podcast/podcast.tsx
+++ src/pages/podcast/podcast.tsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from "react";
 import HeaderAccueil from "../../components/header_footer/headerAccueil";
 import ParallaxOcean from "../../components/ParallaxOcean";
@@ -6,11 +5,20 @@ import ParallaxOcean from "../../components/ParallaxOcean";
 const Podcast: React.FC = () => {
 
     return (
+    
         <div className="flex-col flex items-center justify-start w-full max-w-[100vw] overflow-x-hidden relative">
             <HeaderAccueil />
 
-            <a href="https://www.youtube.com/watch?v=Zvz2Jz9oT2k" target="_blank" rel="noreferrer">podcast 1
-            </a>
+            <h3 className="text-3xl font-bold text-center pt-24">Podcast : Florian Sevellec</h3>
+            <video width="320" height="240" controls >
+                <source src="Florian Sevellec.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <h3 className="text-3xl font-bold text-center pt-24">Podcast : Frederic Le Moigne </h3>
+            <video width="320" height="240" controls >
+                <source src="FredericleMoigne.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
         </div>
     );
 };
