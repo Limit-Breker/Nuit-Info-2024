1eef3062d54ea36731129ced446b0a048740dc1c
username-Everam <simsimi.armand@gmail.com>
2024-12-06 04:48:55 +0100

    h

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Accueil from './pages/accueil/accueil';
 import Test from './pages/test/test';
 import Credit from './pages/credit/Credit';
+import Podcast from './pages/podcast/podcast';
 
 const App: React.FC = () => {
     return (
@@ -16,6 +17,7 @@ const App: React.FC = () => {
                     <Routes>
                         <Route path="/" element={<Accueil />} />
                         <Route path="/credit" element={<Credit />} />
+                        <Route path="/podcast" element={<Podcast />} />
                         <Route path="*" element={<h1>404 - Page non trouvée</h1>} />
                     </Routes>
                 </div>
