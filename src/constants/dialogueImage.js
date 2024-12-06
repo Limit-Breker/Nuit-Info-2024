5ba96471f69d834758cc09034c0ef4f9da9e392d
unkn0wnd.fbx <titouan.helbert@gmail.com>
2024-12-06 04:04:02 +0100

    dialogue finish

---
+++ src/constants/dialogueImage.js
@@ -0,0 +1,13 @@
+const blueHeart = `${process.env.PUBLIC_URL}/images/dialogue/blue-heart.png`;
+const blueImunity = `${process.env.PUBLIC_URL}/images/dialogue/blue-imunity.png`;
+const coralKidney = `${process.env.PUBLIC_URL}/images/dialogue/coral-kidney.png`;
+const greenLungs = `${process.env.PUBLIC_URL}/images/dialogue/green-lungs.png`;
+const orangeLiver = `${process.env.PUBLIC_URL}/images/dialogue/orange-liver.png`;
+
+export const dialogueImage = {
+    blueHeart,
+    blueImunity,
+    coralKidney,
+    greenLungs,
+    orangeLiver,
+};
